0e43e43ec9e21b5c6ba8c0cef90689148d7e5b3d
ardit <zhaozhifei77@gmail.com>
2025-09-02 02:16:32 +1000

    add layout

--- room-booking-website/final-6-after-server-actions/app/account/layout.js
+++ room-booking-website/final-6-after-server-actions/app/account/layout.js
@@ -1,5 +1,8 @@
 import SideNavigation from "@/app/_components/SideNavigation";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function Layout({ children }) {
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
